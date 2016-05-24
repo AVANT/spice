f57b8ad1f2dc554ed6aa45806e4d1a0f5268970d
Sam Hart <sam@hxrts.com>
2016-05-23 20:40:27 -0400

    separate keys from watch script

---
+++ watch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# include keys
+. ".keys"
+
+# precompile paths
+static=$AVANT/alpha/static/
+layouts=$AVANT/alpha/layouts/
+content=$AVANT/alpha/content/
+archetypes=$AVANT/alpha/archetypes/
+src=$AVANT/alpha/src/
+objects=$AVANT/objects/
+
+# print status
+printf "watching directories:\n$static\n$layouts\n$content\n$archetypes\n$src\n$objects\n\n"
+
+# build loop
+while inotifywait -qre modify "$static" "$layouts" "$content" "$archetypes" "$objects" "$src"; do
+
+	# compile css
+	stylus src/styl/world.styl -o static/styles/world.css --compress
+
+	# update public status
+	cat $AVANT/status/head.txt > $AVANT/status/html/index.html
+	date "+%H:%M:%S   %d/%m/%y" >> $AVANT/status/html/index.html
+	echo '<br>' >> $AVANT/status/html/index.html
+	hugo 2>&1 | tee >(sed 's/$/<br>/' >> $AVANT/status/html/index.html) 
+	cat $AVANT/status/tail.txt >> $AVANT/status/html/index.html
+
+	# port json index for search
+	cp public/json/index.html static/site-index.json
+
+	# set cloudflare development mode to true
+	curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cloudflare_zone/settings/development_mode" \
+		-H "X-Auth-Email: $cloudflare_user" \
+		-H "X-Auth-Key: $cloudflare_key" \
+		-H "Content-Type: application/json" \
+		--data '{"value":"on"}' | \
+		jq . > /dev/null
+done
