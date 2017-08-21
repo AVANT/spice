1b2745d594e645dcd2b1fce6fa244a7212a82c75
Sam Hart <sam@hxrts.com>
2017-08-21 16:36:47 -0400

    continue stipping down framework

--- static/js/main.js
+++ static/js/main.js
@@ -1,111 +1,15 @@
-//------------------
-// menu button logic
-//------------------
-
-// $(document).ready(function() {
-
-// 	var $menuIcon = document.getElementsByClassName('logo-link')[0],
-// 		$navBack = document.getElementsByClassName('nav-background')[0],
-// 		$offCanva = document.getElementsByClassName('nav-wrapper')[0],
-// 		$siteWrap = document.getElementsByClassName('site-wrapper')[0];
-
-// 	$menuIcon.addEventListener('click', function() {
-// 		toggleClass($menuIcon, 'open');
-// 		toggleClass($navBack, 'open');
-// 		toggleClass($offCanva, 'open');
-// 		toggleClass($siteWrap, 'open');
-// 	}, false);
-
-// 	$navBack.addEventListener('click', function() {
-// 		toggleClass($menuIcon, 'open');
-// 		toggleClass($navBack, 'open');
-// 		toggleClass($offCanva, 'open');
-// 		toggleClass($siteWrap, 'open');
-// 	}, false);
-
-// 	$menuIcon.addEventListener('mouseenter', function() {
-// 		addClass($menuIcon, 'hover');
-// 		addClass($navBack, 'hover');
-// 	});
-
-// 	$menuIcon.addEventListener('mouseleave', function() {
-// 		removeClass($menuIcon, 'hover');
-// 		removeClass($navBack, 'hover');
-// 	});
-
-// 	function addClass(element, className) {
-// 		element.className += " " + className;
-// 	}
-
-// 	function removeClass(element, className) {
-// 		// capture surrounding whitespace characters
-// 		var classNameRegEx = new RegExp("\\s*" + className + "\\s*");
-// 		element.className = element.className.replace(classNameRegEx, " ");
-// 	}
-
-// 	function toggleClass(element, className) {
-// 		if (!element || !className) {
-// 			return;
-// 		}
-
-// 		if (element.className.indexOf(className) === -1) {
-// 			addClass(element, className);
-// 		} else {
-// 			removeClass(element, className);
-// 		}
-// 	}
-// });
-
-
 //-----------------------------
 // add visibility class on load
 //-----------------------------
 
 (setTimeout(function(){
-	$('.site-wrapper').addClass('load');
+	$('main').addClass('load');
 }, 200));
 
 
-//-----------------------------
-// in-page anchor scroll offset
-//-----------------------------
-
-// function to apply page scroll offset
-function offsetAnchor() {
-	if(location.hash.length !== 0) {
-		window.scrollTo(window.scrollX, window.scrollY - 50);
-	}
-}
-
-// capture hash changes while on page
-$(window).on("hashchange", function () {
-	offsetAnchor();
-});
-
-// navigate to page with a hash also uses offset
-window.setTimeout(function() {
-	offsetAnchor();
-}, 1);
-
-
-//-------------------------------
-// article cover image dimensions
-//-------------------------------
-
-function setDimensions(){
-	var windowsHeight = $(window).height();
-	$('.cover').css('height', windowsHeight - 240 + 'px');
-}
-
-setDimensions();
-
-// when resizing the site adjust section heights
-$(window).resize(function() {
-	setDimensions();
-});
-
-
-
+//-------------
+// lazy loading
+//-------------
 
 function bLazy() {
 	Blazy ({
@@ -143,14 +47,14 @@ $(document).ready(function() {
 	$(document).ready(function() {
 
 		var ias = jQuery.ias({
-			container:  '#posts',
+			container:  '.posts',
 			item:       '.post',
 			pagination: '#pagination',
 			next:       '.next'
 		});
 
 		ias.extension(new IASSpinnerExtension({ html: '<div class="spinner load"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>', }));
-		ias.extension(new IASNoneLeftExtension({ html: '<div class="spinner">∎</div><script>footer = document.getElementById("footer");try { footer.style.opacity = "1"; footer.style.height = "280px"; } catch(e) {"waiting for footer"}</script>' }));
+		ias.extension(new IASNoneLeftExtension({ html: '<div class="spinner">∎</div><script>foot = document.getElementById("foot");try { foot.style.opacity = "1"; foot.style.height = "280px"; } catch(e) {"waiting for footer"}</script>' }));
 
 		ias.on('loaded', function(data, items) {
 			var Bphrase = new bPhrase();
@@ -170,6 +74,7 @@ $(document).ready(function() {
 
 var tiempos_headline = new FontFaceObserver('Tiempos Headline');
 var tiempos_text = new FontFaceObserver('Tiempos Text');
+var programm = new FontFaceObserver('Gerstner Programm');
 
 
 //-------------
@@ -234,23 +139,14 @@ $(document).ready(function() {
 });
 
 
-//----------------------------
-// accomodate iOS touch events
-//----------------------------
-
-if ('ontouchstart' in document) {
-	$('.no-touch').removeClass('no-touch');
-}
-
-
-//-----------------
-// detect ul length
-//-----------------
+//--------------------
+// taxonomy formatting
+//--------------------
 
 
 $(document).ready(function(){
 
-	$('.taxonomy-list ul').each(function() {
+	$('.terms-list').each(function() {
 		var listLength = $(this).children().size();
 		if(listLength >= '20'){
 			$(this).addClass('multi-col');
@@ -258,8 +154,8 @@ $(document).ready(function(){
 	});
 
 	var letList = { letters: [] };  // object to collect the li elements and a list of initial letters
-	$('.taxonomy-list .multi-col').children('li').each(function(){
-		var itmLetter = $(this).children('a').first().text().substring(0,1).toUpperCase();
+	$('.terms-list.multi-col').children('li').each(function(){
+		var itmLetter = $(this).children('.term-title').first().text().substring(0,1).toUpperCase();
 		if (!(itmLetter in letList)) {
 			letList[itmLetter] = [];
 			letList.letters.push(itmLetter);
@@ -282,7 +178,7 @@ $(document).ready(function(){
 
 		var cap = $(ul).prepend($('<li/>').attr('name', letter.toLowerCase()).addClass('list-title').html(letter));
 		console.log(cap);
-		$('.taxonomy-list .multi-col').append(cap);  // add the list to a new li
+		$('.terms-list').append(cap);  // add the list to a new li
 
 	});
 
@@ -296,14 +192,8 @@ $(document).ready(function(){
 
 Page = function() {
 
-	// word breaking
-	if (typeof bPhrase == 'function') { var Bphrase = new bPhrase(); }
-
 	// lazy load images
 	if (typeof bLazy == 'function') { var Blazy = new bLazy(); }
-
-	// tiled layout
-	if (typeof Iso == 'function') { var iso = new Iso(); }
 
 };
 
@@ -318,5 +208,22 @@ $(document).ready(function(){
 	$.getScript("/js/vendor/hanging-punctuation.min.js", function() {
 		$.holdReady(false);
 	});
+
+});
+
+
+//----------------------------------------
+// add class on fractional viewport scroll
+//----------------------------------------
+
+$(window).scroll(function() {
+
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 0.5 * $(window).height()) {
+		$(".site-map-base-links").addClass('hide');
+	} else {
+		$(".site-map-base-links").removeClass('hide');
+	}
 
 });
